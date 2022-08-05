1adf5c64ce4ac40e3d78b606b6707abe6e9ca98e
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-08-05 12:40:03 +0200

    Update scrollto.js

--- js/scrollto.js
+++ js/scrollto.js
@@ -1,6 +1,7 @@
 jQuery(function ($) {
     $.scrollTo(0);
-    $('#contact').click(function () { $.scrollTo($('#Tocontact'), 1000); });
+    $('#contact_header').click(function () { $.scrollTo($('#Tocontact'), 1000); });
+    $('#contact_footer').click(function () { $.scrollTo($('#Tocontact'), 1000); });
 
     $('#scrollUp').click(function () { $.scrollTo($('body'), 1000); });
 }
