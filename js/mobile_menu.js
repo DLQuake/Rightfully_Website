a9b0a3268dfcc11417d98223194462e74644cb31
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-08-05 11:50:37 +0200

    Add jQuery for tests

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,6 +1,15 @@
+// open and close mobile menu in javascript
 const hamburger = document.getElementById('hamburger');
 const navul = document.getElementById('navUL');
 
 hamburger.addEventListener('click', () => {
     navul.classList.toggle('show');
 });
+
+
+// the same thing but in jquery
+// $(document).ready(function () {
+//     $("#hamburger").click(function () {
+//         $("#navUL").slideToggle(500).css("display", "flex");
+//     });
+// });
