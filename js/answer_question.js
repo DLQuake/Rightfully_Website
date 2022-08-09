ffac7560c6a1e6ca8aad9d2fbeaf9260c0a3b8da
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-08-09 11:31:44 +0200

    Update js files with jQuery

--- js/answer_question.js
+++ js/answer_question.js
@@ -1,28 +1,20 @@
-//! show and hide answer question in javascript
-const questions = document.getElementsByClassName('frequently-asked-questions__questions--question');
-const answers = document.getElementsByClassName('frequently-asked-questions__questions--answer');
+$('.frequently-asked-questions__questions-question').on("click", function () {
+    $(this).toggleClass('active');
+});
 
-for (let i = 0; i < questions.length; i++) {
-    questions[i].addEventListener('click', () => {
-        answers[i].classList.toggle('show');
-    });
-}
-
-
-//! the same thing but in jquery
-// $(document).ready(function () {
-//     $("#question1").click(function () { $("#answer1").slideToggle(500); });
-//     $("#question2").click(function () { $("#answer2").slideToggle(500); });
-//     $("#question3").click(function () { $("#answer3").slideToggle(500); });
-//     $("#question4").click(function () { $("#answer4").slideToggle(500); });
-//     $("#question5").click(function () { $("#answer5").slideToggle(500); });
-//     $("#question6").click(function () { $("#answer6").slideToggle(500); });
-//     $("#question7").click(function () { $("#answer7").slideToggle(500); });
-//     $("#question8").click(function () { $("#answer8").slideToggle(500); });
-//     $("#question9").click(function () { $("#answer9").slideToggle(500); });
-//     $("#question10").click(function () { $("#answer10").slideToggle(500); });
-//     $("#question11").click(function () { $("#answer11").slideToggle(500); });
-//     $("#question12").click(function () { $("#answer12").slideToggle(500); });
-//     $("#question13").click(function () { $("#answer13").slideToggle(500); });
-//     $("#question14").click(function () { $("#answer14").slideToggle(500); });
-// });
+$(document).ready(function () {
+    $("#question1").click(function () { $("#answer1").slideToggle(500); });
+    $("#question2").click(function () { $("#answer2").slideToggle(500); });
+    $("#question3").click(function () { $("#answer3").slideToggle(500); });
+    $("#question4").click(function () { $("#answer4").slideToggle(500); });
+    $("#question5").click(function () { $("#answer5").slideToggle(500); });
+    $("#question6").click(function () { $("#answer6").slideToggle(500); });
+    $("#question7").click(function () { $("#answer7").slideToggle(500); });
+    $("#question8").click(function () { $("#answer8").slideToggle(500); });
+    $("#question9").click(function () { $("#answer9").slideToggle(500); });
+    $("#question10").click(function () { $("#answer10").slideToggle(500); });
+    $("#question11").click(function () { $("#answer11").slideToggle(500); });
+    $("#question12").click(function () { $("#answer12").slideToggle(500); });
+    $("#question13").click(function () { $("#answer13").slideToggle(500); });
+    $("#question14").click(function () { $("#answer14").slideToggle(500); });
+});
--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,15 +1,5 @@
-// open and close mobile menu in javascript
-const hamburger = document.getElementById('hamburger');
-const navul = document.getElementById('navUL');
-
-hamburger.addEventListener('click', () => {
-    navul.classList.toggle('show');
+$(document).ready(function () {
+    $("#hamburger").click(function () {
+        $("#navUL").slideToggle(200).css("display", "flex");
+    });
 });
-
-
-// the same thing but in jquery
-// $(document).ready(function () {
-//     $("#hamburger").click(function () {
-//         $("#navUL").slideToggle(500).css("display", "flex");
-//     });
-// });
